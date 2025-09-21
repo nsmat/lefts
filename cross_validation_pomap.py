dcd39f09d97f54822f7632a6b82a9ea680232c7f
nikmather <niksmather@gmail.com>
2025-09-21 11:39:07 +0200

    fix randomised cross validation

--- cross_validation_pomap.py
+++ cross_validation_pomap.py
@@ -6,43 +6,32 @@ class RandomisedCrossValidationPoMap(Pomap):
 
     def __init__(self, num_folds: int, index_column: 'str'):
         super().__init__(name=f'Randomised CV: {index_column}')
-        # TODO needs a 'labels' method
         self.num_folds = num_folds
         self.index_column = index_column
         self.fold_labels = [str(c) for c in range(num_folds)]
         self._test_label_mapping = {}
+
+    def labels(self) -> pl.DataFrame:
+        return pl.Series(values=range(self.num_folds)).to_frame()
 
     def index_to_label(self, label):
         if label not in self._test_label_mapping:
             self._test_label_mapping[label] = choice(self.fold_labels)
         return self._test_label_mapping[label]
 
-    def label_rows_as_test(self, df: pl.DataFrame, label: dict):
+    def train_label_expr(self, df: pl.DataFrame, label):
         assert df.unique(self.index_column).shape[0] == df.shape[0]
 
         # Every row is randomly assigned to one of the possible folds
         all_indexes = df.select(self.index_column).unique()
         index_to_label_dict = {index: self.index_to_label(index) for index in all_indexes}
 
-        df = df.with_columns(
-            pl.col(self.index_column)
-            .replace_strict(index_to_label_dict)
-            .alias(self._train_column_name(label))
-        )
+        expr = pl.col(self.index_column).replace_strict(index_to_label_dict) == label
 
-        return df
+        return expr
 
     def label_rows_as_train(self, df, label):
-        df = self.label_rows_as_test(df, label)
-        test_column = self._test_column_name(label)
-
-        df = df.with_columns(
-            ~(pl.col(test_column))
-            .alias(self._train_column_name(label))
-        )
-        df = df.drop(test_column)
-
-        return df
+        return ~self.train_label_expr(df, label)
 
 
 
